cbbde486b76957ff51fc9c9f0fe009f1ab3cb245
Motoki Mera <motokimera@yahoo.co.jp>
2023-07-28 15:24:27 +0900

    TEST

---
+++ price = 468.py
@@ -0,0 +1,8 @@
+price = 468
+pay = 10000
+money_type = [10000,5000,1000,500,100]
+charge_total = pay - price
+for money in money_type:
+    qiot = charge_total // money
+    print(money,'en= ',qiot,'mai', sep= '')
+    charge_total -= qiot * money
